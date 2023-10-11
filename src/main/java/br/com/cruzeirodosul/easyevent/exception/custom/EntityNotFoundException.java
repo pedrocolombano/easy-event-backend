3832dd44bb22ca29106c0a1282a697cd33d7df7a
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-10-11 09:49:30 -0300

    feat: add custom exception for entity not found

---
+++ src/main/java/br/com/cruzeirodosul/easyevent/exception/custom/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.cruzeirodosul.easyevent.exception.custom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
