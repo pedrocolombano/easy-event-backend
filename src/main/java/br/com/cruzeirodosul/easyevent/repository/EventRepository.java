bddf5461d7f98363be2b91d046c8535c5497d59e
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-10-11 09:28:56 -0300

    refactor: update entity id

--- src/main/java/br/com/cruzeirodosul/easyevent/repository/EventRepository.java
+++ src/main/java/br/com/cruzeirodosul/easyevent/repository/EventRepository.java
@@ -4,8 +4,6 @@ import br.com.cruzeirodosul.easyevent.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
